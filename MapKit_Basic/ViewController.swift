b552464ca9ad7fca1e0c14f7daba91dcffbd0014
leonardo15043 <lahernandez15043@gmail.com>
2018-10-06 20:47:16 -0500

    Agregando una anotacion

--- MapKit_Basic/ViewController.swift
+++ MapKit_Basic/ViewController.swift
@@ -22,6 +22,15 @@ class ViewController: UIViewController {
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         
         centerMapOnLocation(location: initialLocation)
+        
+        
+     
+        let artwork = Artwork(title: "King David Kalakaua",
+                              locationName: "Waikiki Gateway Park",
+                              discipline: "Sculpture",
+                              coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        mapView.addAnnotation(artwork)
+
  
     }
     
